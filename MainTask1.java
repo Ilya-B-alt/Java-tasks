40c03a3f9e5f6a7b5b09d6ec88fb35231e13f48b
Ilya Babaryka <boboriko2010@mail.ru>
2021-08-19 03:13:45 +0300

    first task

---
+++ MainTask1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MainTask1 {
+     public static void main(String[] args) {
+
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter your name, please");
+             String name = scanner.nextLine();
+             System.out.println("Hello " + name);
+             scanner.close();
+     }
+ }
+
+
+
+
+
+
