1808a7ac2fa77b2fa1afdea4b4e533262ca1d912
Ilya Babaryka <boboriko2010@mail.ru>
2021-08-20 20:08:30 +0300

    second java fundamentals main task

---
+++ JavaFundamentals/MainTask2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class MainTask2 {
+
+    public static void main(String[] args) {
+
+           for (int i = args.length-1; i >=0; i--)
+           System.out.print(args[i] + " ");
+
+        }
+    }
+
+
+
+
