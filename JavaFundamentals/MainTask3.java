7cb463901b0c0d0f9cfc433ac9b7f54a84ddedba
Ilya Babaryka <boboriko2010@mail.ru>
2021-08-21 02:29:38 +0300

    third task

---
+++ JavaFundamentals/MainTask3.java
@@ -0,0 +1,25 @@
+package com.company;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainTask3 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any number, please");
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            int [] array = new int[number];
+            Random random = new Random();
+            for (int element : array) {
+                array[element] = random.nextInt(number-1);
+                System.out.println(array[element]);
+                //System.out.print(array[element] + " ");
+            }
+        } else { System.out.println("NAN"); }
+
+    }
+}
